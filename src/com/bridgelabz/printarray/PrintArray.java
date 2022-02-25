444a48e9ae6fa1982b997c35a5b53b7201698d5f
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-25 17:29:35 +0530

    Added : passing all in same constructor

--- src/com/bridgelabz/printarray/PrintArray.java
+++ src/com/bridgelabz/printarray/PrintArray.java
@@ -1,45 +1,48 @@
 /**
- * UC3 :PROBLEM STATEMENT
- * Given an array of Integer, Double and Character, write a program to print the UC 2 same using Generics Class
- */
-
-/**
  * @author Suraj Chaudhary
  * @date 25-Feb-2022
  */
-// integer array printing
+
 package com.bridgelabz.printarray;
 
-public class PrintArray<T> {
+public class PrintArray<X, Y, Z> {
+	private X[] myXArray;
+	private Y[] myYArray;
+	private Z[] myZArray;
 
-	private T[] inputArray;
-
-	public PrintArray(T[] inputArray) {
-		this.inputArray = inputArray;
+//constructor
+	public PrintArray(X[] myXArray, Y[] myYArray, Z[] myZArray) {
+		super();
+		this.myXArray = myXArray;
+		this.myYArray = myYArray;
+		this.myZArray = myZArray;
 	}
 
-	public void toPrint() {
-		PrintArray.toPrint(this.inputArray);
-	}
-
-	private static <E> void toPrint(E[] inputArray) {
-		for (E element : inputArray) {
-			System.out.println(element);
-		}
+	// generic array printing
+	static public <T> void printArray(T[] arr) {
+		for (T element : arr)
+			System.out.print(element + " ");
 		System.out.println();
 	}
 
+	// generic array printing using class
+
+	private void printArray() {
+		printArray(myXArray);
+		printArray(myYArray);
+		printArray(myZArray);
+	}
+
 	public static void main(String[] args) {
-
-		System.out.println("__________Welcome to Printing Array using Java Generics___________");
-
-		Integer[] integerArray = { 1, 2, 3 };
-		Double[] doubleArray = { 1.2, 1.2, 1.3 };
-		Character[] characterArray = { 'a', 's', 'h', 'i', 'k', 'a' };
-
-		new PrintArray(integerArray).toPrint();
-		PrintArray.toPrint(doubleArray);
-		PrintArray.toPrint(characterArray);
+		Integer[] arr1 = { 10, 20, 30 };
+		Double[] arr2 = { 1.23, 2.24, 3.345 };
+		Character[] arr3 = { 'a', 'A', 'V' };
+		/// using generic methods
+		printArray(arr1);
+		printArray(arr2);
+		printArray(arr3);
+		/// passing all types at same time
+		new PrintArray<Integer, Double, Character>(arr1, arr2, arr3).printArray();
 
 	}
 
