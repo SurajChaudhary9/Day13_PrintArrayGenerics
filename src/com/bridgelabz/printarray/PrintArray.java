c188c9584c51b107720d10e5c27205a9c0b428e9
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-25 17:11:47 +0530

    Added : Print Array three variables using Generics

--- src/com/bridgelabz/printarray/PrintArray.java
+++ src/com/bridgelabz/printarray/PrintArray.java
@@ -1,10 +1,6 @@
 /**
- * UC1 :PROBLEM STATEMENT
- * Given an array of Integer,
-Double and Character, write
-a program to print the same - Create PrintArray class and define
-toPrint method to print
-corresponding elements to stdout
+ * UC2 :PROBLEM STATEMENT
+ * Given an array of Integer, Double and Character, write a program to print the UC 2 same using Generics
  */
 
 /**
@@ -15,39 +11,21 @@ corresponding elements to stdout
 package com.bridgelabz.printarray;
 
 public class PrintArray {
-	private static void toPrint(Integer[] a) {
-
-		for (int element : a) {
+	private static <t> void toPrint(t[] inputArray) {
+		for (t element : inputArray) {
 			System.out.println(element);
 		}
-
-	}
-
-	// double array printing
-	private static void toPrint(Double[] b) {
-
-		for (double element : b) {
-			System.out.println(element);
-		}
-
-	}
-
-	// character array printing
-	private static void toPrint(Character[] c) {
-
-		for (char element : c) {
-			System.out.println(element);
-		}
-
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
 
-		System.out.println("_______________Welcome to Printing Array using Java Generics ________________");
+		System.out.println("___________Welcome to Printing Array using Java Generics___________");
 
 		Integer[] a = { 1, 2, 3 };
 		Double[] b = { 1.2, 1.2, 1.3 };
 		Character[] c = { 'a', 's', 'h', 'i', 'k', 'a' };
+
 		toPrint(a);
 		toPrint(b);
 		toPrint(c);
